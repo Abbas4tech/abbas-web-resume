2a743e763a5f0d4e55fc899073c6ffd8b3df8858
Abbas4tech <shaikhabbas54@gmail.com>
2024-07-07 17:50:51 +0530

    🔧 refactor: added configs settings

--- app/utils/data.ts
+++ app/utils/data.ts
@@ -1,9 +1,7 @@
+import { contentfulConfig } from "../config/contentful";
 import { Theme } from "./enums";
 import { ContentFulInfo, Themes } from "./types";
 
 export const themes: Themes = [Theme.DARK, Theme.LIGHT];
 
-export const contentful: ContentFulInfo = {
-  accessToken: "7Xqn0FowV95IGZGEx72dFzslmBHvIFNaYLEyEDMVHB0",
-  space: "6mdmgsjzhh4y",
-};
+export const contentful: ContentFulInfo = contentfulConfig;
