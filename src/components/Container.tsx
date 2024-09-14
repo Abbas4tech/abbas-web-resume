9b60fa82909a34125c14ff078df154b293ba564b
Abbas4tech <shaikhabbas54@gmail.com>
2024-09-15 02:07:15 +0530

    ✨ feat: added dynamic page routing

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,14 +1,31 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
 import ProfileCard from "./ProfileCard";
 import * as AOS from "aos";
+import { usePathname, useRouter } from "next/navigation";
+import { useUserInfo } from "@context/useInfo";
+
 interface ContainerProps {
   children: React.ReactNode;
 }
 
 const Container = ({ children }: ContainerProps) => {
+  const { pages } = useUserInfo();
+  const router = useRouter();
+  const [next, setNext] = useState<string>("");
+  const currentPath =
+    usePathname().charAt(1).toUpperCase() + usePathname().slice(2);
+  const nextPage = pages[(pages.indexOf(currentPath) + 1) % pages.length];
+  useEffect(() => {
+    setNext(nextPage);
+  }, [currentPath]);
+
+  const changePage = () => {
+    router.push(`/${next.toLowerCase()}`);
+  };
+
   AOS.init();
   return (
     <main className="w-full container overflow-hidden h-screen md:text-lg text-sm mx-auto">
@@ -18,7 +35,9 @@ const Container = ({ children }: ContainerProps) => {
         <div className="drawer-content mx-4 mt-4 mb-[6rem] overflow-auto">
           <ProfileCard />
           {children}
-          <button className="btn btn-primary mb-14">Change Page</button>
+          <button onClick={changePage} className="btn btn-primary mb-14">
+            {next}
+          </button>
         </div>
         <Sidebar />
       </div>
