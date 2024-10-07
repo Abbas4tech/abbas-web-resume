6ce62643001b8aadadb2ebfe0c2c99473fbe3fae
Abbas4tech <shaikhabbas54@gmail.com>
2024-10-07 08:31:01 +0530

    ⚡️ feat: imoroved structure and previous UI bug fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,38 +2,34 @@
 import React, { memo } from "react";
 import { ThemeSwitch, Resume as ResumeComponent } from "@components";
 import { useApplicationData } from "@context/useApplication";
-import { usePathname } from "next/navigation";
-import RouteClientWrapper from "./RouteClientWrapper";
 
 const Header = memo(() => {
   const { title, defaultPage } = useApplicationData();
 
   return (
-    <header className="bg-base-100 text-base-content sticky top-0 z-30 flex w-full justify-center bg-opacity-90 backdrop-blur transition-shadow duration-100 [transform:translate3d(0,0,0)]">
+    <header className="container mx-auto bg-base-100 text-base-content sticky top-0 z-30 flex w-full justify-center bg-opacity-90 backdrop-blur transition-shadow duration-100 [transform:translate3d(0,0,0)]">
       <nav className="navbar bg-base-100 h-10 lg:h-20">
         <div className="navbar-start gap-2">
-          <RouteClientWrapper className="lg:hidden" defaultRoute={defaultPage}>
-            <label
-              tabIndex={0}
-              className={"btn btn-ghost btn-circle drawer-button"}
-              htmlFor="my-drawer-2"
+          <label
+            tabIndex={0}
+            className={"btn btn-ghost btn-circle drawer-button lg:hidden"}
+            htmlFor="my-drawer-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
-            </label>
-          </RouteClientWrapper>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h7"
+              />
+            </svg>
+          </label>
 
           <button className="text-lg normal-case lg:text-2xl p-2 btn btn-ghost">
             {title}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,13 +5,17 @@ import Link from "next/link";
 import { SVGIcon } from "@components";
 import { useApplicationData } from "@context/useApplication";
 
-const Sidebar = memo(() => {
+interface SidebarProps {
+  changePage: () => void;
+}
+
+const Sidebar = memo(({ changePage }: SidebarProps) => {
   const { pages, defaultPage } = useApplicationData();
   const currentPath = usePathname().slice(1) || defaultPage.toLowerCase();
 
   return (
     <>
-      <div className="drawer-side top-16 lg:top-0">
+      <div className="drawer-side lg:top-0 top-16">
         <label
           htmlFor="my-drawer-2"
           aria-label="close sidebar"
@@ -28,7 +32,7 @@ const Sidebar = memo(() => {
             >
               <Link
                 scroll={false}
-                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+                onClick={changePage}
                 href={`/${[
                   page.toLowerCase() === defaultPage.toLowerCase()
                     ? ""
