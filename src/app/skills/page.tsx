7b7e6535f1dcc29f40fe3ec969cc053fd5ea2af8
Abbas4tech <shaikhabbas54@gmail.com>
2025-03-06 00:39:30 +0530

    ✨ feat: added skills component chnages

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -4,6 +4,15 @@ import { DynamicIcon, SkillSetItem } from "@components";
 import { SkillsPage as SkillsPageSchema } from "@lib/contentful";
 import { fetchPageMetadata, fetchQuery } from "@lib/api";
 import { Page, PageContent, PageHeading } from "src/components/ui/page";
+import {
+  Skill,
+  SkillGroupContent,
+  SkillsContent,
+  SkillTitle,
+  SkillGroup,
+  SkillList,
+} from "src/components/ui/skill";
+import Progress from "src/components/ui/progress";
 
 export const generateMetadata = async (): Promise<Metadata> => {
   return fetchPageMetadata<SkillsPageSchema>(
@@ -33,8 +42,27 @@ const SkillsPage: NextPage = async () => {
         className="flex flex-col gap-4"
         contentAnimation={contentAnimation}
       >
-        {skillsSet.map((item, index: number) => (
-          <SkillSetItem {...item} key={index} />
+        {skillsSet.map(({ icon, title, skillsArray }, index: number) => (
+          <Skill key={index}>
+            <SkillsContent>
+              <SkillTitle>
+                <DynamicIcon {...icon} />
+                {title}
+              </SkillTitle>
+              <SkillGroup>
+                {skillsArray.map(({ title, skillIcons, skillProgress }) => (
+                  <SkillGroupContent key={title}>
+                    <SkillList>
+                      {skillIcons.map((skill) => (
+                        <DynamicIcon key={title} {...skill} />
+                      ))}
+                    </SkillList>
+                    <Progress skillProgress={skillProgress} />
+                  </SkillGroupContent>
+                ))}
+              </SkillGroup>
+            </SkillsContent>
+          </Skill>
         ))}
       </PageContent>
     </Page>
--- src/components/skills/SkillSet.tsx
+++ src/components/skills/SkillSet.tsx
@@ -13,10 +13,10 @@ const SkillSetItem: React.FC<SkillSet> = ({
       className="mockup-window bg-base-300 border-neutral border"
     >
       <div className="p-4 bg-base-200">
-        <div className="flex items-center mb-4 text-xl font-bold md:text-3xl">
+        <h1 className="flex text-xl font-bold items-center md:text-3xl gap-4 mb-4 ">
           <DynamicIcon {...icon} />
-          <h1 className="flex ml-4">{title}</h1>
-        </div>
+          {title}
+        </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-2 md:p-4">
           {skillsArray.map((res: SkillGroup, i: number) => (
             <SkillGroupItem {...res} key={i} />
