fdeaafff1b02dc9681f596dfe47d3ab78439147f
Abbas4tech <shaikhabbas54@gmail.com>
2026-06-18 01:05:15 +0530

    chore: added timeline sections props handling

--- src/components/blocks/timeline-section/timeline-section.adapter.ts
+++ src/components/blocks/timeline-section/timeline-section.adapter.ts
@@ -11,17 +11,33 @@ export function adaptTimelineSection(
   return {
     animation: undefined,
     entries: data.customEntries.map((item) => {
-      const metaRows: { icon: { iconCode: string }; text: string }[] = [];
+      const metaRows: TimelineSectionProps["entries"][number]["metaRows"] = [];
       if (item.startDate || item.endDate) {
         metaRows.push({
-          icon: { iconCode: "calendar" },
-          text: `${item.startDate?.getFullYear() || ""} - ${item.endDate?.getFullYear() || "Present"}`,
+          icon: { iconCode: "md/MdDateRange", name: "Duration", size: "18" },
+          text: `${item.startDate?.toLocaleString("en", { month: "long", year: "numeric" })} - ${item.endDate?.toLocaleString("en", { month: "long", year: "numeric" }) || "Present"}`,
+        });
+      }
+      if (item.description) {
+        metaRows.push({
+          icon: { iconCode: "fa/FaMapMarkerAlt", name: "Place", size: "18" },
+          text: item.description,
         });
       }
       if (item.subtitle) {
         metaRows.push({
-          icon: { iconCode: "briefcase" },
+          icon: { iconCode: "io5/IoPerson", name: "Role", size: "18" },
           text: item.subtitle,
+        });
+      }
+      if (item.tags && item.tags.length > 0) {
+        metaRows.push({
+          icon: {
+            iconCode: "fa/FaStackOverflow",
+            name: "Tech Stack",
+            size: "18",
+          },
+          text: item.tags.join(", "),
         });
       }
 
